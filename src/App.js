39bc0b109d18a8ec143a0eb7d1c66393c373d704
Liam <liam.alton@signaturegroup.com.au>
2024-02-15 10:28:01 +1100

    example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import { CognitoUserPool, AuthenticationDetails, CognitoUser} from 'amazon-cognito-identity-js';
 
 function App() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const poolData = {
+    UserPoolId: "",
+    ClientId: ""
+  }
+
+  const userPool = new CognitoUserPool(poolData)
+  const authenticationDetails = new AuthenticationDetails({
+    Username : email,
+    Password : password,
+  });
+  const userData = {
+    Username : email,
+    Pool : userPool
+  };
+  var cognitoUser = new CognitoUser(userData);
+
+
+  const onSubmitLogin = event => {
+    event.preventDefault()
+    console.log('Submitting Login...')
+    cognitoUser.authenticateUser(authenticationDetails, {
+      onSuccess: function (result) {
+        var accessToken = result.getAccessToken().getJwtToken();
+
+        /* Use the idToken for Logins Map when Federating User Pools with identity pools or when passing through an Authorization Header to an API Gateway Authorizer */
+        var idToken = result.idToken.jwtToken;
+        console.log("ACCESS_TOKEN",accessToken)
+        console.log("JWT_TOKEN",idToken)
+      },
+      onFailure: function(err) {
+          console.error(err);
+      },
+    })
+  }
+
+  const onSubmitSignUp = event => {
+    event.preventDefault()
+    console.log('Submitting Sign Up...')
+    userPool.signUp(email,password,[],null,(err,data) => {
+      if(err) console.error(err)
+
+      console.log(data)
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>LOGIN</h2>
+      <form onSubmit={onSubmitLogin}>
+        <input 
+          value={email} 
+          onChange={event => setEmail(event.target.value)} 
+          type="text"
+        />
+        <input 
+          value={password} 
+          onChange={event => setPassword(event.target.value)}
+          type="text"
+        />
+        <button>Login</button>
+      </form>
+      <br></br>
+      <h2>SIGN UP</h2>
+      <form onSubmit={onSubmitSignUp}>
+        <input 
+          value={email} 
+          onChange={event => setEmail(event.target.value)} 
+          type="text"
+        />
+        <input 
+          value={password} 
+          onChange={event => setPassword(event.target.value)}
+          type="text"
+        />
+        <button>Sign Up</button>
+      </form>
     </div>
+    
   );
 }
 
